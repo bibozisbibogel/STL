ce75d621b3acb94c0f0ddb44af60114a4b42dc1b
Stoian Cristian Georgian <93210010+bibozisbibogel@users.noreply.github.com>
2024-12-10 20:59:53 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,96 +2,103 @@
 #include <fstream>
 #include <string>
 #include <vector>
-#include <algorithm>
+#include <algorithm> 
+#define FAST ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 using namespace std;
-
-struct Doctor
-{
-    std::string name;
-    std::string speciality;
+struct doctors {
+    string nume;
+    int nr_specializari = 0;
     int start = 9;
     int finish = 17;
-    int solved_problems = 0;
-    vector<string>boli;
+    vector<string>specializari;
+    vector<pair<string, int>>boli;
+    int pacienti_rezolvati = 0;
 };
-struct Pacient
-{
-    std::string name;
-    std::string disease;
-    int time_to_fix;
+struct problems {
+    string problema;
+    string specialitateNecesara;
+    int durata;
     int priority_number;
+    int ora_sosire;
 };
-
-bool operator==(const Pacient& p1, const Doctor& d2) {
-    if (p1.disease == d2.speciality) {
-        return true;
-    }
-    else return false;
-}
-
-int main()
+/// overloaded function
+bool operator==(const problems& p, const doctors& d)
 {
-    std::ifstream inFile("input.txt");
-
-    int no_problems, no_doctors, time_to_solve, nr_prioritate;
-    std::string name, speciality;
-    std::vector<Doctor> doctors;
-    std::vector<Pacient> pacients;
-
+    for (auto& specializare : d.specializari) {
+        if (p.specialitateNecesara == specializare)
+            return 1;
+    }
+    return 0;
+}
+int main(){
+    vector<problems> probleme;
+    vector<doctors> doctori;
+    ifstream inFile("input4_bonus.txt");
+    int no_problems, no_doctors, arrival_time,nr_prioritate,duration;
+    string name, speciality;
     inFile >> no_problems;
-
-    for (int i = 0; i < no_problems; i++) {
+    for (int i = 0; i < no_problems; i++)
+    {
         inFile >> name;
         inFile >> speciality;
-        inFile >> time_to_solve;
+        inFile >> arrival_time;
+        inFile >> duration;
         inFile >> nr_prioritate;
-        Pacient p;
-        p.name = name;
-        p.disease = speciality;
-        p.time_to_fix = time_to_solve;
-        p.priority_number = nr_prioritate;
-        pacients.emplace_back(p);
+        probleme.emplace_back(name, speciality,duration,nr_prioritate,arrival_time);
     }
-
+    int specializarile_doctorului;
     inFile >> no_doctors;
-
-    for (int i = 0; i < no_doctors; i++) {
+    for (int i = 0; i < no_doctors; i++)
+    {
         inFile >> name;
-        inFile >> speciality;
-
-        Doctor d;
-        d.name = name;
-        d.speciality = speciality;
-        d.start = 9;
-        d.finish = 17;
-        doctors.emplace_back(d);
+        inFile >> specializarile_doctorului;
+        int incepere = 9;
+        int terminare = 17;
+        vector<string>Specializare_Doctor;
+        Specializare_Doctor.clear();
+        for (int j = 0; j < specializarile_doctorului; j++)
+        {
+            string numele_domeniului;
+            inFile >> numele_domeniului;
+            Specializare_Doctor.emplace_back(numele_domeniului);
+        }
+        doctori.emplace_back(name, specializarile_doctorului, incepere, terminare, Specializare_Doctor);
     }
-    /// sortam in functie de prioritate
-    sort(pacients.begin(), pacients.end(), [](Pacient a, Pacient b) {
-        return a.priority_number > b.priority_number;
+    /// sortam dupa ora_sosire si daca sunt egale, sortam dupa prioritate (Descrescator)
+    sort(probleme.begin(), probleme.end(), [](problems a, problems b)
+        {
+            if (a.ora_sosire != b.ora_sosire)
+                return a.ora_sosire < b.ora_sosire;
+            return a.priority_number > b.priority_number;
         });
-    for (auto& pacient : pacients) {
-        auto it = find_if(doctors.begin(), doctors.end(), [&pacient](const Doctor& doc) {
-            if (doc.speciality == pacient.disease and doc.start+pacient.time_to_fix<=doc.finish)
-                return true;
-            else 
-                return false;
+    for (auto& pacient : probleme)
+    {
+        auto it = find_if(doctori.begin(), doctori.end(), [&pacient](const doctors& d) {
+            if (d.start <= pacient.ora_sosire && pacient.ora_sosire + pacient.durata <= d.finish) // are timp
+            {
+                for (int k = 0; k < d.nr_specializari; k++)
+                {
+                    if (d.specializari[k] == pacient.specialitateNecesara)
+                        return 1;
+                }
+            }
+            return 0;
             });
-
-        if (it != doctors.end()) {
-            (*it).start += pacient.time_to_fix;
-            (*it).solved_problems++;
-            (*it).boli.emplace_back(pacient.name);
+        if (it != doctori.end()) // am gasit
+        {
+            (*it).start = pacient.ora_sosire + pacient.durata;
+            (*it).pacienti_rezolvati++;
+            (*it).boli.emplace_back(pacient.problema, pacient.ora_sosire);
         }
     }
-    for (auto doc : doctors)
+    for (auto& d : doctori)
     {
-        if (doc.solved_problems != 0)
+        if (d.pacienti_rezolvati != 0)
         {
-            cout << doc.name << " " << doc.solved_problems << " ";
-            for (auto boala : doc.boli)
+            cout << d.nume << " " << d.pacienti_rezolvati << " ";
+            for (auto& pereche : d.boli)
             {
-                cout << boala << " ";
+                cout << pereche.first << " " << pereche.second << " ";
             }
             cout << '\n';
         }
